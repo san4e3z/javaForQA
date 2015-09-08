013a777007b3d693c2c81dad9a8560c491f143e2
Alex <San4e3z2010@yandex.ru>
2015-09-08 10:33:25 +0300

    Task_1

---
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -0,0 +1,37 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class GroupCreationTests extends TestBase {
+	
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.name = "group name 1";
+    group.header = "header 1";
+    group.footer = "footer 1 ";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
